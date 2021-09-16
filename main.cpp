093b428fd7169c7a7d12e2013c8a015fde258807
Vlad Sidorov <mylongmind@gmail.com>
2021-09-16 16:36:01 +0300

    Template refactor

--- main.cpp
+++ main.cpp
@@ -33,9 +33,9 @@ using namespace std;
 
 void fast_io()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 }
 
 void file_in(const string& filename)
@@ -48,16 +48,15 @@ void file_out(const string& filename)
     freopen(filename.c_str(), "w", stdout);
 }
 
-double diffclock(clock_t clock1, clock_t clock2)
+double diff_clock(clock_t clock1, clock_t clock2)
 {
-    double diffticks = clock2 - clock1;
-    return diffticks / ((double)CLOCKS_PER_SEC / 1000);
+    return (double)(clock2 - clock1) / CLOCKS_PER_SEC / 1000;
 }
 
 void sleep(int milliseconds)
 {
     clock_t start = clock();
-    for (clock_t end = clock(); diffclock(start, end) < milliseconds; end = clock()) {}
+    for (clock_t end = clock(); diff_clock(start, end) < milliseconds; end = clock()) {}
 }
 
 
@@ -79,8 +78,7 @@ signed main()
     solve();
 
 #ifdef LOCAL
-    cerr << "TIME = " << (clock() - start) / ((double)CLOCKS_PER_SEC / 1000) << '\n';
+    cerr << "TIME = " << diff_clock(start, clock()) << '\n';
 #endif
-
     return 0;
 }
