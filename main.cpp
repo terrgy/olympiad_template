3591b6424c287c812da826dba5a000f2cf500ac8
Vlad Sidorov <mylongmind@gmail.com>
2021-09-29 17:25:47 +0300

    Add data printing for debug

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@
 
 #ifdef LOCAL
 #define _GLIBCXX_DEBUG
+#else
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("O3")
 #endif
 
 #include <iostream>
@@ -19,7 +22,25 @@
 #include <ctime>
 #include <vector>
 #include <iomanip>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <bitset>
+#include <list>
+#include <forward_list>
+#include <tuple>
+#include <string>
+#include <complex>
 #include <random>
+#include <array>
+#include <sstream>
 
 #define len(a) (int)a.size()
 #define all(a) a.begin(), a.end()
@@ -78,6 +99,146 @@ ll randint64(ll a, ll b)
     static mt19937_64 rnd(rd());
     return (ll)(rnd() % (b - a + 1) + a);
 }
+
+struct ToStringConverter
+{
+    template<typename T>
+    static string convert(const T& t)
+    {
+        return to_string(t);
+    }
+
+    static string convert(const char* c)
+    {
+        return convert((string)c);
+    }
+
+    static string convert(const char& c)
+    {
+        ostringstream stream;
+        stream << '\'' << c << '\'';
+        return stream.str();
+    }
+
+    static string convert(const string& str)
+    {
+        return "\"" + str + "\"";
+    }
+
+    static string convert(const bool& b)
+    {
+        return b ? "true" : "false";
+    }
+
+    template<typename T>
+    static string convert(const set<T>& st)
+    {
+        ostringstream stream;
+        stream << "set[";
+        int index = 0;
+        for (const T& i : st)
+        {
+            stream << convert(i);
+            if (++index == (int)st.size())
+            {
+                stream << ']';
+            }
+            else
+            {
+                stream << ", ";
+            }
+        }
+        return stream.str();
+    }
+
+    template<typename T>
+    static string convert(const vector<T>& v)
+    {
+        ostringstream stream;
+        stream << "vector[";
+        for (int i = 0; i < (int)v.size(); ++i)
+        {
+            stream << convert(v[i]);
+            if (i == (int)v.size() - 1)
+            {
+                stream << ']';
+            }
+            else
+            {
+                stream << ", ";
+            }
+        }
+        return stream.str();
+    }
+
+    template<typename Key, typename Value>
+    static string convert(const map<Key, Value>& mp)
+    {
+        ostringstream stream;
+        stream << "map{";
+        int index = 0;
+        for (const auto& [key, value] : mp)
+        {
+            stream << convert(key) << ": " << convert(value);
+            if (++index == (int)mp.size())
+            {
+                stream << '}';
+            }
+            else
+            {
+                stream << ", ";
+            }
+        }
+        return stream.str();
+    }
+
+    template<int I, typename... Args>
+    struct TupleConverter
+    {
+        static string tuple_convert(const tuple<Args...>& t)
+        {
+            ostringstream stream;
+            stream << TupleConverter<I - 1, Args... >::tuple_convert(t);
+            stream << convert(get<I>(t)) << (I == tuple_size_v< tuple<Args...> > - 1 ? ")" : ", ");
+            return stream.str();
+        }
+    };
+
+    template<typename... Args>
+    struct TupleConverter<0, Args...>
+    {
+        static string tuple_convert(const tuple<Args...>& t)
+        {
+            ostringstream stream;
+            stream << '(' << convert(get<0>(t)) << (tuple_size_v< tuple<Args...> > == 1 ? ")" : ", ");
+            return stream.str();
+        }
+    };
+
+    template<typename... Args>
+    static string convert(const tuple<Args...>& t)
+    {
+        return TupleConverter< tuple_size_v< tuple<Args...> > - 1, Args...>::tuple_convert(t);
+    }
+};
+
+void debug_out()
+{
+    cerr << endl;
+}
+
+template<typename Head, typename... Tail>
+void debug_out(Head head, Tail... tail)
+{
+    cerr << ToStringConverter::convert(head) << "; ";
+    debug_out(tail...);
+}
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]: ", debug_out(__VA_ARGS__)
+#else
+#define debug(...)
+#endif
 
 void solve()
 {
