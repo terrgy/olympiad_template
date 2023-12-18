2e4595dc09d404256412ede7d7de4afb6ecefa2c
Vlad Sidorov <mylongmind@gmail.com>
2023-12-18 22:00:59 +0300

    Refactor Kun's algorithm

--- Code/Algorithms/Graphs/Matchings/Kuns algorithm.cpp
+++ Code/Algorithms/Graphs/Matchings/Kuns algorithm.cpp
@@ -1,27 +1,53 @@
-vector< vector<int> > graph;
-vector<char> used;
-vector<int> parent;
+struct KunsAlgorithm {
+    vector< vector<int> >& first_part_graph;
+    int second_part_size;
+    vector<int> second_part_matching;
+    vector<char> used;
+    vector<char> first_part_flag;
 
-bool kun_dfs(int v)
-{
-    used[v] = 1;
-    for (int to : graph[v])
-    {
-        if ((parent[to] == -1) || (!used[parent[to]] && kun(parent[to])))
-        {
-            parent[to] = v;
-            return true;
+    KunsAlgorithm(vector< vector<int> >& first_part_graph, int second_part_size) :
+            first_part_graph(first_part_graph), second_part_size(second_part_size),
+            second_part_matching(second_part_size, -1), first_part_flag(first_part_graph.size()) {}
+
+
+    bool dfs(int v) {
+        used[v] = 1;
+        for (int to : first_part_graph[v]) {
+            if ((second_part_matching[to] == -1) ||
+                (!used[second_part_matching[to]] && dfs(second_part_matching[to]))) {
+                second_part_matching[to] = v;
+                return true;
+            }
         }
+        return false;
     }
-    return false;
-}
 
-void kun()
-{
-    parent.assign(m, -1);
-    for (int i = 0; i < n; ++i)
-    {
-        used.assign(n, 0);
-        kun_dfs(i);
+    int make_simple_matches() {
+        int matchings_found = 0;
+        for (int i = 0; i < first_part_graph.size(); ++i) {
+            if (first_part_flag[i]) {
+                continue;
+            }
+            for (int to : first_part_graph[i]) {
+                if (second_part_matching[to] == -1) {
+                    second_part_matching[to] = i;
+                    ++matchings_found;
+                    first_part_flag[i] = 1;
+                    break;
+                }
+            }
+        }
+        return matchings_found;
     }
-}
+
+    int find_matchings() {
+        int matchings_found = make_simple_matches();
+        for (int i = 0; i < first_part_graph.size(); ++i) {
+            if (!first_part_flag[i]) {
+                used.assign(first_part_graph.size(), 0);
+                matchings_found += dfs(i);
+            }
+        }
+        return matchings_found;
+    }
+};
